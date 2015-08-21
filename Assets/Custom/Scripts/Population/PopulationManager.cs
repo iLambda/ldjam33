71910188e1b43917b808a90c7257d50587e67677
iLambda <ilambda@hotmail.fr>
2015-08-22 00:58:57 +0200

    Added PopulationManager

---
+++ Assets/Custom/Scripts/Population/PopulationManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PopulationManager : MonoBehaviour {
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    
+	}
+}
