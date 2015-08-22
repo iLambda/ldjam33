e4567a9ebf682eee23024e1f82a3a858dd17cfb2
Julien Noble <penguzine@gmail.com>
2015-08-22 02:19:42 +0200

    citizen prefab + georges

--- Assets/Custom/Scripts/Population/PopulationManager.cs
+++ Assets/Custom/Scripts/Population/PopulationManager.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System;
 
 public class PopulationManager : MonoBehaviour {
-    
+    public GameObject CitizenPrefab;
+
+    public int PopulationCount = 200;
+    public int RandomSeed = Random.Range(int.MinValue, int.MaxValue);
+
+    public List<Transform> Children;
+
+
 	void Start () {
-	
+        Random.seed = RandomSeed;
+        for (int c = 0; c < PopulationCount; c++)
+        {
+            var citizen = Instantiate(CitizenPrefab, new Vector3(Random.Range(-45f, 45f), 0.5f, Random.Range(-45f, 45f)), Quaternion.Euler(0, 0, 0)) as GameObject;
+            citizen.transform.SetParent(transform);
+        }
 	}
 	
 	void Update () {
