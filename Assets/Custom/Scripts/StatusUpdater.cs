3cd3db1e48389cc3182a5f0c8f904cb98954d11e
vimock <vi_mock@hotmail.com>
2015-08-23 16:56:03 +0200

    Add statusupdater

---
+++ Assets/Custom/Scripts/StatusUpdater.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatusUpdater : MonoBehaviour {
+
+	public static int zombiesCount = 0;
+	public static int humanCount = 0;
+	public static int timeCount = 0;
+	public static int contaminatedCount = 0;
+
+	private UnityEngine.UI.Text zombiesCountText;
+	private UnityEngine.UI.Text humansCountText;
+	private UnityEngine.UI.Text timeCountText;
+	private UnityEngine.UI.Text contaminatedCountText;
+
+	// Use this for initialization
+	void Start () {
+
+		zombiesCountText = (UnityEngine.UI.Text) GameObject.Find ("ZombiesCountText").GetComponent<Text>(); 
+		humansCountText = (UnityEngine.UI.Text) GameObject.Find ("HumansCountText").GetComponent<Text>(); 
+		timeCountText = (UnityEngine.UI.Text) GameObject.Find ("TimeCountText").GetComponent<Text>(); 
+		contaminatedCountText = (UnityEngine.UI.Text) GameObject.Find ("ContaminedCountText").GetComponent<Text>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		zombiesCountText.text = StatusUpdater.zombiesCount.ToString ();
+		humansCountText.text = StatusUpdater.humanCount.ToString ();
+		timeCountText.text = StatusUpdater.timeCount.ToString ();
+		contaminatedCountText.text = StatusUpdater.contaminatedCount.ToString ();
+	}
+
+
+}
