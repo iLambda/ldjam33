cd37dedc5352e2788b7769fbac0eb63965b6a364
Lucile <lucile@ymail.com>
2015-08-23 14:39:53 +0200

    Deflector can be heard by zombies
    NEXT : Deflector will broadcast deflection message to zombies

---
+++ Assets/Custom/Scripts/Population/Potentials/Deflector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+
+class Deflector : MonoBehaviour
+{
+    //attributes
+    float hearRange = 5.0f; // TODO adjust this value
+    int zombiesLayer = 1 << LayerMask.NameToLayer("zombieHearing");
+
+    public void Update()
+    {
+        //play sound here
+        Collider[] zombiesWhoHeard = Physics.OverlapSphere(transform.position, hearRange, zombiesLayer);
+        Debug.Log("There are " + zombiesWhoHeard.Length + " hearing me");
+
+        foreach (Collider enemy in zombiesWhoHeard)
+        {
+            // use either Send Message or GetComponent and tell this enemy he heard something
+        }
+
+    }
+}
