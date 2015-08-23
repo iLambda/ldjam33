96992d7a0fb62e20f23fb1549e74205f7e4300d1
Lucile <lucile@ymail.com>
2015-08-23 22:40:06 +0200

    ApocalipseManager does now only instanciates zombies in the bottom of the screen

--- Assets/Custom/Scripts/ApocalypticPopManager.cs
+++ Assets/Custom/Scripts/ApocalypticPopManager.cs
@@ -14,32 +14,40 @@ public class ApocalypticPopManager : MonoBehaviour {
 	public int HumanSpawn = 50;
     // The seed used
     public int RandomSeed = 0;
-    // The boundaries
-    public Vector3 worldBoundsMin = new Vector3(-25, -1, -25);
-    public Vector3 worldBoundsMax = new Vector3(25, -1, 25);
-    
+        
 	void Start () 
     {
+        // The boundaries
+        Camera mainCamera = Camera.main;
+        Vector3 cameraPosition = mainCamera.transform.position;
+        float xDist = mainCamera.aspect * mainCamera.orthographicSize;
+        float zDist = mainCamera.orthographicSize;
+        float xMax = cameraPosition.x + xDist/2;
+        float xMin = cameraPosition.x - xDist/2;
+        float zMax = cameraPosition.z;
+        float zMin = cameraPosition.z - zDist;
+
+
         if (RandomSeed == 0)
             RandomSeed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
         // Setting the seed
         UnityEngine.Random.seed = RandomSeed;
 
 
-        // Spawning humans
+        /*// Spawning humans
 		for (int c = 0; c < HumanSpawn; c++)
         {
             // Spawning a citizen
             var citizen = Instantiate(HumanPrefab, new Vector3(UnityEngine.Random.Range(worldBoundsMin.x, worldBoundsMax.x), -1.0f, UnityEngine.Random.Range(worldBoundsMin.z, worldBoundsMax.z)), Quaternion.Euler(0, 0, 0)) as GameObject;
 			// Set as a parent270
             citizen.transform.SetParent(transform);
-        }
+        }*/
         
 		
-		// Spawning humans
+		// Spawning zombies
 		for (int c = 0; c < ZombieSpawn; c++)
         {
-			var citizen = Instantiate(ZombiePrefab, new Vector3(UnityEngine.Random.Range(worldBoundsMin.x, worldBoundsMax.x), -1.0f, UnityEngine.Random.Range(worldBoundsMin.z, worldBoundsMax.z)), Quaternion.Euler(0, 0, 0)) as GameObject;
+			var citizen = Instantiate(ZombiePrefab, new Vector3(UnityEngine.Random.Range(xMin, xMax), -1.0f, UnityEngine.Random.Range(zMin, zMax)), Quaternion.Euler(0, 0, 0)) as GameObject;
 
             // Set as a parent
             citizen.transform.SetParent(transform);
