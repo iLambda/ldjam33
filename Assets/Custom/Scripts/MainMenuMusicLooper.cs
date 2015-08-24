fb0d8412316a64f6e5594a93f2a9ea12e4ea825d
vimock <vi_mock@hotmail.com>
2015-08-24 22:10:39 +0200

    Add music to main menu

---
+++ Assets/Custom/Scripts/MainMenuMusicLooper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuMusicLooper : MonoBehaviour {
+
+	public AudioClip[] stings;
+	public AudioSource stingSource;
+	public float timeBetweenLoop;
+	public float waitLoop;
+	private int current;
+
+	// Use this for initialization
+	void Start () {
+		current = 0;
+		waitLoop = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waitLoop -= Time.deltaTime;
+		if (waitLoop < 0)
+		{
+			if (!stingSource.isPlaying) 
+			{
+				stingSource.clip = stings[current];
+				stingSource.Play();
+				current++;
+				if (current >= stings.Length) 
+				{
+					current = 0;
+					waitLoop = timeBetweenLoop + stingSource.clip.length;
+				}
+			}
+		}
+	}
+}
