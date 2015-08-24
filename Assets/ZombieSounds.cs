893b52b359f769d5b6765747c577e1d4667beff4
Lucile <lucile@ymail.com>
2015-08-25 00:36:37 +0200

    Added new sounds

---
+++ Assets/ZombieSounds.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class ZombieSounds : MonoBehaviour
+{
+    public AudioSource gunSource;
+    public bool awaken = false;
+
+    public void Awake()
+    {
+        awaken = true;
+        gunSource.Play();
+    }
+
+    public void Update()
+    {
+        gunSource.Play();
+    }
+    public void OnDestroy()
+    {
+        if (awaken)
+        {
+            Debug.Log("Should trigger sound");
+            gunSource.Play();
+        }
+    }
+
+
+}
